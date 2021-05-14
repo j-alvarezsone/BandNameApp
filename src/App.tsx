0a2e5c70db499acd9ef1a8589645d554bff31c43
Jorge Alvarez Sone <j.alvarezsone@gmail.com>
2021-05-14 23:20:35 +0200

    connected socket, and added BandList

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,74 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
+import { AddBand } from './compoenents/AddBand';
+import { BandList } from './compoenents/BandList';
+import { Bands } from './types/types';
+
+import io from 'socket.io-client';
+
+const connectSocketServer = () => {
+  const socket = io.connect('http://localhost:8080', {
+    transports: ['websocket'],
+  });
+  return socket;
+};
 
 function App() {
+  const [socket] = useState(connectSocketServer());
+  const [online, setOnline] = useState<boolean>(false);
+  const [bands, setBands] = useState<Bands[]>([]);
+
+  useEffect(() => {
+    setOnline(socket.connected);
+  }, [socket]);
+
+  useEffect(() => {
+    socket.on('connect', () => {
+      setOnline(true);
+    });
+  }, [socket]);
+
+  useEffect(() => {
+    socket.on('disconnect', () => {
+      setOnline(false);
+    });
+  }, [socket]);
+
+  useEffect(() => {
+    socket.on('current-bands', (bands: []) => {
+      setBands(bands);
+    });
+  }, [socket]);
+
+  const vote = (id: string) => {
+    console.log('votar - app', id);
+    socket.emit('vote-band', id);
+  };
+
   return (
-    <div>
-      <h1>Hola mundo</h1>
+    <div className='container'>
+      <div className='alert'>
+        <p>
+          Service Status:
+          {online ? (
+            <span className='text-success'> Online</span>
+          ) : (
+            <span className='text-danger'> Offline</span>
+          )}
+        </p>
+      </div>
+
+      <h1>BandNames</h1>
+      <hr />
+
+      <div className='row'>
+        <div className='col-8'>
+          <BandList data={bands} vote={vote} />
+        </div>
+
+        <div className='col-4'>
+          <AddBand />
+        </div>
+      </div>
     </div>
   );
 }
