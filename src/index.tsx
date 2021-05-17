7e8f654f3694a69d4a6f99607eb7252cc67af707
Jorge Alvarez Sone <j.alvarezsone@gmail.com>
2021-05-17 14:09:59 +0200

    added HomePage

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import { BandNamesApp } from './BandNamesApp';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<BandNamesApp />, document.getElementById('root'));
